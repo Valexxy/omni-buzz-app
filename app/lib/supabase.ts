3540b7a7612ccc6693270f69b91daf2e22584ff4
Valexxy <101820260+Valexxy@users.noreply.github.com>
2026-03-24 01:52:50 +0100

    Update supabase.ts

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+// This client is used for the frontend to read news
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
